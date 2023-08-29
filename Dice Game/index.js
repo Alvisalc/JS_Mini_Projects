0f6bc6638262b3bce0e568960a593d47c700cce8
Alvisalc <alvisccy1107@gmail.com>
2023-08-29 12:57:52 -0400

    add comments

--- Dice Game/index.js
+++ Dice Game/index.js
@@ -4,17 +4,20 @@ var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
 var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
 
-var image1 = document.querySelectorAll("img")[0];
+var image1 = document.querySelectorAll("img")[0]; // 1st dice - Get the first dice element
 
-image1.setAttribute("src", randomImageSource);
+image1.setAttribute("src", randomImageSource); // 1st dice - Set the source attribute
 
+// Generate another random number between 1 and 6
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
+// 2nd dice - Build the full image source path
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
+// 2nd dice - Set the source attribute
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
-
+// Compare the random numbers and find the winner
 if (randomNumber1 > randomNumber2){
     document.querySelector("h1").innerHTML = "Player 1 Wins!";
 }
