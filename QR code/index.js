6a3c018d0177ea853228b087402d46ea688edfa9
Alvisalc <alvisccy1107@gmail.com>
2023-08-29 13:27:25 -0400

    readme and comments to projects

--- QR code/index.js
+++ QR code/index.js
@@ -1,16 +1,22 @@
 
-
+// Get references
 let imgBox = document.getElementById("imgBox");
 let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
 
+// Function to generate a QR code
 function generateQR(){
+        // Check the input text for generating QR code
         if(qrText.value.length > 0){
+        // QR code image URL api
         qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+        // Add show image to display the QR code
         imgBox.classList.add("show-img");
     }else{
+        // Add error if no input text
         qrText.classList.add('error');
         setTimeout(()=>{
+            // Remove the error after 1000ms (1 second)
             qrText.classList.remove('error');
         },1000);
     }
--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 <h3>This repository stores many interesting mini-projects.</h3>
 
-<p>Dice Game - <i>comments added</i></p>
-<p>DOM Challenge</p>
-<p>Drum Kit</p>
-<p>jQuery</p>
-<p>Password Generator</p>
-<p>Weather App</p>
-<p>To-Do List</p>
-<p>Crypto Web</p>
-<p>QR Code</p>
+<p>Dice Game - <i>comments added.</i></p>
+<p>DOM Challenge - <i>in process...</i></p>
+<p>Drum Kit - <i>comments added.</i></p>
+<p>jQuery - <i>comments added.</i></p>
+<p>Password Generator - <i>comments added, inline JS.</i></p>
+<p>Weather App - <i>comments added, inline JS, require api access key.</i></p>
+<p>To-Do List - <i>comments added.</i></p>
+<p>Crypto Web - <i>comments added.</i></p>
+<p>QR Code - <i>comments added.</i></p>
 <p>Image Search - <i>comments added, require api access key.</i></p>
